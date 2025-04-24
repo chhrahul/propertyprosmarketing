0d23d3c7ca111bc3c32ac2e06b64a6bd5c624987
Heena <chh.odesk.mca@gmail.com>
2025-04-24 16:51:03 +0530

    fixed design issue

--- src/views/pages/auth/ForgetPassword.vue
+++ src/views/pages/auth/ForgetPassword.vue
@@ -11,7 +11,7 @@ const password = ref('');
     <FloatingConfigurator />
     <div class="bg-surface-50 dark:bg-surface-950 flex items-center justify-center min-h-screen min-w-[100vw] overflow-hidden">
         <div class="flex flex-col items-center justify-center">
-            <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
+            <div style="border-radius: 56px; padding: 0.3rem; width: 100%; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
                 <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20" style="border-radius: 53px">
                     <div>
                         <label for="email1" class="block text-surface-900 dark:text-surface-0 text-xl font-medium mb-2">Email</label>
--- src/views/payoutDetails.vue
+++ src/views/payoutDetails.vue
@@ -7,100 +7,105 @@
         </nav>
 
         <h2 class="text-2xl font-semibold">Payout Details</h2>
-        <p class="text-gray-600 mt-1 mb-4">Updated <span class="font-medium"> {{ formatDate(payoutsData.paid_at)}}</span></p>
+        <p class="text-gray-600 mt-1 mb-4">Updated <span class="font-medium"> {{
+            formatDate(payoutsData.paid_at) }}</span></p>
 
-        <table class="w-full border rounded-lg mb-5">
-            <thead class="bg-gray-100 text-gray-700">
-                <tr>
-                    <th class="p-3 text-left">State</th>
-                    <th class="p-3 text-left">Amount</th>
-                    <th class="p-3 text-left">Paid at</th>
-                    <th class="p-3 text-left">Paid by</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-if="loadingPayouts">
-                    <td colspan="4" class="py-4 text-center text-gray-500">
-                        <div class="flex justify-center items-center">
-                            <svg class="animate-spin h-6 w-6 text-blue-500 mr-2" xmlns="http://www.w3.org/2000/svg"
-                                fill="none" viewBox="0 0 24 24">
-                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                                    stroke-width="4"></circle>
-                                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
-                            </svg>
-                            <span>Loading Payouts...</span>
-                        </div>
-                    </td>
-                </tr>
-                <tr class="border-t" v-if="payoutsData.length !== 0">
-                    <td class="p-4 text-left">
-                        <span
-                            class="inline-flex items-center rounded-md bg-green-50 px-3 py-1 text-sm font-medium text-green-700 ring-1 ring-green-600/20 ring-inset">
-                            {{ capitalize(payoutsData.state) }}
-                        </span>
-                    </td>
-                    <td class="p-4">{{ payoutsData.amount / 100 }} {{ payoutsData.currency }}</td>
-                    <td class="p-4">{{ formatDate(payoutsData.paid_at) || 'N/A' }}</td>
-                    <td class="p-4">{{ payoutsData.paid_by_id }}</td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="overflow-x-auto">
+            <table class="w-full border rounded-lg mb-5">
+                <thead class="bg-gray-100 text-gray-700">
+                    <tr>
+                        <th class="p-3 text-left">State</th>
+                        <th class="p-3 text-left">Amount</th>
+                        <th class="p-3 text-left">Paid at</th>
+                        <th class="p-3 text-left">Paid by</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-if="loadingPayouts">
+                        <td colspan="4" class="py-4 text-center text-gray-500">
+                            <div class="flex justify-center items-center">
+                                <svg class="animate-spin h-6 w-6 text-blue-500 mr-2" xmlns="http://www.w3.org/2000/svg"
+                                    fill="none" viewBox="0 0 24 24">
+                                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                        stroke-width="4"></circle>
+                                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
+                                </svg>
+                                <span>Loading Payouts...</span>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="border-t" v-if="payoutsData.length !== 0">
+                        <td class="p-4 text-left">
+                            <span
+                                class="inline-flex items-center rounded-md bg-green-50 px-3 py-1 text-sm font-medium text-green-700 ring-1 ring-green-600/20 ring-inset">
+                                {{ capitalize(payoutsData.state) }}
+                            </span>
+                        </td>
+                        <td class="p-4">{{ payoutsData.amount / 100 }} {{ payoutsData.currency }}</td>
+                        <td class="p-4">{{ formatDate(payoutsData.paid_at) || 'N/A' }}</td>
+                        <td class="p-4">{{ payoutsData.paid_by_id }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 
-        <h5 class="text-lg font-semibold mt-5">Commissions Included in This Payout</h5>
-        <table class="w-full border rounded-lg mt-3">
-            <thead class="bg-gray-100 text-gray-700">
-                <tr>
-                    <th class="p-3 text-left">Description</th>
-                    <th class="p-3 text-left">Date</th>
-                    <th class="p-3 text-left">Customer</th>
-                    <th class="p-3 text-left">Sale Amount</th>
-                    <th class="p-3 text-left">Your Commission</th>
-                    <th class="p-3 text-left">Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-if="loadingCommission">
-                    <td colspan="6" class="py-4 text-center text-gray-500">
-                        <div class="flex justify-center items-center">
-                            <svg class="animate-spin h-6 w-6 text-blue-500 mr-2" xmlns="http://www.w3.org/2000/svg"
-                                fill="none" viewBox="0 0 24 24">
-                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                                    stroke-width="4"></circle>
-                                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
-                            </svg>
-                            <span>Loading Commissions...</span>
-                        </div>
-                    </td>
-                </tr>
-                <tr v-for="commission in commissionsData" :key="commission.id">
-                    <td class="p-4 text-left">
-                        {{ commission.sale.description }}
-                        <span class="block text-sm text-gray-500">
-                            1 x {{ commission.sale.description }} (at {{ commission.sale.sale_amount_cents / 100
-                            }}/each)
-                        </span>
-                    </td>
-                    <td class="p-4 text-left">{{ formatDate(commission.created_at) }}</td>
-                    <td class="p-4 text-left">
-                        <span class="font-mono text-sm text-gray-600">
-                            {{ commission.sale.referral.customer.name || commission.sale.referral.id }}
-                        </span>
-                    </td>
-                    <td class="p-4 text-left">
-                        ${{ commission.sale.sale_amount_cents / 100 }} {{ commission.currency }}
-                    </td>
-                    <td class="p-4 text-left">
-                        ${{ commission.amount / 100 }} {{ commission.currency }}
-                    </td>
-                    <td class="p-4 text-left">
-                        <span :class="statusClasses[commission.state]"
-                            class="inline-flex items-center rounded-md px-3 py-1 text-sm font-medium ring-1 ring-inset">
-                            {{ capitalize(commission.state) }}
-                        </span>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+    <h5 class="text-lg font-semibold mt-5">Commissions Included in This Payout</h5>
+    <div class="overflow-x-auto">
+    <table class="w-full border rounded-lg mt-3">
+        <thead class="bg-gray-100 text-gray-700">
+            <tr>
+                <th class="p-3 text-left">Description</th>
+                <th class="p-3 text-left">Date</th>
+                <th class="p-3 text-left">Customer</th>
+                <th class="p-3 text-left">Sale Amount</th>
+                <th class="p-3 text-left">Your Commission</th>
+                <th class="p-3 text-left">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-if="loadingCommission">
+                <td colspan="6" class="py-4 text-center text-gray-500">
+                    <div class="flex justify-center items-center">
+                        <svg class="animate-spin h-6 w-6 text-blue-500 mr-2" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4">
+                            </circle>
+                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
+                        </svg>
+                        <span>Loading Commissions...</span>
+                    </div>
+                </td>
+            </tr>
+            <tr v-for="commission in commissionsData" :key="commission.id">
+                <td class="p-4 text-left">
+                    {{ commission.sale.description }}
+                    <span class="block text-sm text-gray-500">
+                        1 x {{ commission.sale.description }} (at {{ commission.sale.sale_amount_cents / 100
+                        }}/each)
+                    </span>
+                </td>
+                <td class="p-4 text-left">{{ formatDate(commission.created_at) }}</td>
+                <td class="p-4 text-left">
+                    <span class="font-mono text-sm text-gray-600">
+                        {{ commission.sale.referral.customer.name || commission.sale.referral.id }}
+                    </span>
+                </td>
+                <td class="p-4 text-left">
+                    ${{ commission.sale.sale_amount_cents / 100 }} {{ commission.currency }}
+                </td>
+                <td class="p-4 text-left">
+                    ${{ commission.amount / 100 }} {{ commission.currency }}
+                </td>
+                <td class="p-4 text-left">
+                    <span :class="statusClasses[commission.state]"
+                        class="inline-flex items-center rounded-md px-3 py-1 text-sm font-medium ring-1 ring-inset">
+                        {{ capitalize(commission.state) }}
+                    </span>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
     </div>
 </template>
 
@@ -171,7 +176,7 @@ watchEffect(() => {
     console.log("commissionsData :", commissionsData.value);
 });
 
-const formatDate = (date) =>  date ? new Date(date).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' }) : '--';
+const formatDate = (date) => date ? new Date(date).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' }) : '--';
 const capitalize = (str) => str ? str.charAt(0).toUpperCase() + str.slice(1).toLowerCase() : '';
 
 </script>
