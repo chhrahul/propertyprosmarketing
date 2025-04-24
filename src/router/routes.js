5958cfee31ff297262f30cd4260baf08c38648a5
Heena <chh.odesk.mca@gmail.com>
2025-04-24 12:35:07 +0530

    improve routing

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,27 +1,29 @@
 import AppLayout from '@/layout/AppLayout.vue';
+import Dashboard from '@/views/Dashboard.vue';
 import Login from '@/views/pages/auth/Login.vue';
-import ForgetPassword from '@/views/pages/auth/ForgetPassword.vue';
 import homeDash from '@/views/homeDash.vue';
+import commission from '@/views/commission.vue';
 import commissionNew from '@/views/commissionNew.vue';
 import referrals from '@/views/referrals.vue';
 import payouts from '@/views/payouts.vue';
 import profile from '@/views/profile.vue';
 import adminDash from '@/views/adminDash.vue';
+import ForgetPassword from '@/views/pages/auth/ForgetPassword.vue';
 import PayoutDetails from '@/views/payoutDetails.vue';
 
 const routes = [
     {
         path: '/',
         name: 'login',
-        component: Login,
         beforeEnter: (to, from, next) => {
             const isAuthenticated = localStorage.getItem('token');
             if (isAuthenticated) {
-                next('/dashboard');  // Redirect to /dashboard if authenticated
+                next('/dashboard');
             } else {
                 next();
             }
-        }
+        },
+        component: Login
     },
     {
         path: '/auth/forgetpassword',
@@ -34,7 +36,7 @@ const routes = [
         beforeEnter: (to, from, next) => {
             const isAuthenticated = localStorage.getItem('token');
             if (!isAuthenticated) {
-                next('/');  // Redirect to login if not authenticated
+                next('/');
             } else {
                 next();
             }
@@ -42,40 +44,39 @@ const routes = [
         children: [
             {
                 path: '',
-                name: 'homeDash',  // Default child route for /dashboard
+                name: 'homeDash',
                 component: homeDash
             },
             {
-                path: 'commission',
-                name: 'commission',
-                component: commissionNew
-            },
-            {
-                path: 'referrals',
+                path: '/referrals',
                 name: 'referrals',
                 component: referrals
             },
             {
-                path: 'payouts',
+                path: '/payouts',
                 name: 'payouts',
                 component: payouts
             },
             {
-                path: 'profile',
-                name: 'profile',
+                path: '/profile',
+                name: 'Profile',
                 component: profile
             },
             {
-                path: 'adminDash',
+                path: '/adminDash',
                 name: 'adminDash',
                 component: adminDash
             },
             {
-                path: 'PayoutDetails/:id',
+                path: '/PayoutDetails/:id',
                 name: 'PayoutDetails',
                 component: PayoutDetails
             }
         ]
+    },
+    {
+        path: '/:pathMatch(.*)*', // Catch-all route for unmatched paths
+        redirect: '/dashboard' // Redirect to the dashboard or any default route
     }
 ];
 
